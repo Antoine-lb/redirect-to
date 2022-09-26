8a93b37ef62b3a41f16425110896e8cabb110ae6
Antoine-LB <antoine.stc25@gmail.com>
2022-09-26 14:22:19 +0200

    init

---
+++ src/envVariables.ts
@@ -0,0 +1,3 @@
+export const envVariables = {
+  VITE_REDIRECT_TO: import.meta.env.VITE_REDIRECT_TO,
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    	import {envVariables } from '../envVariables';
+        import { browser } from '$app/environment'; 
+
+        if (browser) {
+            window.location.href = envVariables.VITE_REDIRECT_TO;
+
+        }
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
